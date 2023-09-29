a50772062547b8fd2dba0ed9d30c19d38dcd5e4b
nsznsnb <nsznsnb@gmail.com>
2023-09-29 23:42:33 +0900

    等価性比較をテストでも使うように修正

--- TDDPractice.Test/MoneyTest.cs
+++ TDDPractice.Test/MoneyTest.cs
@@ -9,9 +9,9 @@ namespace TDDPractice.Test
         {
             Dollar five = new Dollar(5);
             Dollar product = five.Times(2);
-            Assert.Equal(10, product.Amount);
+            Assert.Equal(new Dollar(10), product);
             product = five.Times(3);
-            Assert.Equal(15, product.Amount);
+            Assert.Equal(new Dollar(15), product);
         }
 
         [Fact]
